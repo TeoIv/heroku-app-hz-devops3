3d1b2d0024a34917df7cbacc765d00461075cc62
TeoIv <90186068+TeoIv@users.noreply.github.com>
2023-06-24 16:32:38 +0200

    faq removed

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -8,7 +8,7 @@
             <li class="{{ Request::is('profile') ? 'active' : '' }}"><a href="/profile">Profile</a></li>
             <li class="{{ Request::is('grades') ? 'active' : '' }}"><a href="{{route('grades.index')}}">Dashboard</a></li>
             <li class="{{ Request::is('posts') ? 'active' : '' }}"><a href="{{ route('posts.index') }}">Blog</a></li>
-            <li class="{{ Request::is('faqs') ? 'active' : '' }}"><a href="{{ route('faqs.index') }}">FAQ</a></li>
+        <!--<li class="{{ Request::is('faqs') ? 'active' : '' }}"><a href="{{ route('faqs.index') }}">FAQ</a></li>-->
             <li class="{{ Request::is('gallery') ? 'active' : '' }}"><a href="/gallery">Gallery</a></li>
             <li class="{{ Request::is('people') ? 'active' : '' }}"><a href="{{ route('people.index') }}">People</a></li>
 
